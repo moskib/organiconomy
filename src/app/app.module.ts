3e56e2446e204afae4c0168cc008ced7bf6ea48e
moskib <30794120+moskib@users.noreply.github.com>
2018-08-02 06:19:29 +0300

    Added Generic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,14 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './products/products.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
+
 
 // Other:
 import { environment } from './../environments/environment';
@@ -21,7 +29,14 @@ import { environment } from './../environments/environment';
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -35,8 +50,32 @@ import { environment } from './../environments/environment';
         component: HomeComponent
       },
       {
+        path: 'products',
+        component: ProductsComponent
+      },
+      {
         path: 'shopping-cart',
         component: ShoppingCartComponent
+      },
+      {
+        path: 'check-out',
+        component: CheckOutComponent
+      },
+      {
+        path: 'order-success',
+        component: OrderSuccessComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'adming/products',
+        component: AdminProductsComponent
+      },
+      {
+        path: 'admin/orders',
+        component: AdminOrdersComponent
       }
     ])
 
