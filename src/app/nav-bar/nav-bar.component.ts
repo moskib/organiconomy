970baf4a3533fed211a7a224be9c492053f96c9b
moskib <30794120+moskib@users.noreply.github.com>
2018-08-04 11:16:40 +0300

    Added async pipe to user observable and fixed crash rlated issues to Bootstrap Angular components

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
   public isCollapsed = false;
   
-  constructor() { }
+  user$: Observable<firebase.User>;
 
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) { 
+    this.user$ = afAuth.authState;
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
