38c7cf57387d50500ae6b28968f110ef4778d22f
moskib <30794120+moskib@users.noreply.github.com>
2018-08-07 14:25:58 +0300

    AddedDeleteToProduct-form

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -30,4 +30,8 @@ export class ProductService {
   update(productId, product) {
     return this.db.object('/products/' + productId).update(product);
   }
+
+  delete(productId) {
+    return this.db.object('/products/' + productId).remove();
+  }
 }
